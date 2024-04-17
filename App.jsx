a1584f3839b810426836eca750a1fe9d6745c2f2
Ritesh Sharma <ritesh.hp.sharma@gmail.com>
2024-04-17 15:18:13 +0530

    added stack navigation

--- App.jsx
+++ App.jsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/components/Home';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+import { Home } from './src';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerRight: () => (<EvilIcons name='navicon' size={30} color={'black'} />)
+        }}
+      >
         <Stack.Screen name="React Native" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
